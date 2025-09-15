13b1a3cbe487a870b18a768da4cd94df1fb37a3c
Gustavo Passos <gusapas26@gmail.com>
2025-09-14 22:42:06 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 
   import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
+  import react from '@vitejs/plugin-react-swc';//-swc
   import path from 'path';
 
   export default defineConfig({
